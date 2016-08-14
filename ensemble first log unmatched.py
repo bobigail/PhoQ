5bbf0bde0e8c0930bcba04b3bf2ebff32f743eec
Bob Grahame <bob@bobigail.com>
2016-08-13 22:58:06 -0700

    Add log unmatched

---
+++ ensemble first log unmatched.py
@@ -0,0 +1,61 @@
+from prody import *
+from pylab import *
+#ion()
+
+# select PDBs to work with
+pdbids = ['1yax', '3bqa', '3bq8', '3cgz', '3cgy', '4uey']
+pdbfiles = fetchPDB(*pdbids, compressed=False)
+
+# print len(pdbfiles)
+
+# identify reference chain
+ref_structure = parsePDB('4uey')
+ref_selection = ref_structure.select('calpha')
+ref_chain = ref_selection.getHierView().getChain('A')
+
+# create ensemble
+startLogfile('PhoQ_pca')
+ensemble = PDBEnsemble('PhoQ')
+
+# define atoms and coordinates for the ensemble reference
+ensemble.setAtoms(ref_chain)
+ensemble.setCoords(ref_chain)
+
+for pdbid in pdbids:
+    
+    hv = parsePDB(pdbid).getHierView()
+    for chain in hv:
+        structure = parsePDB(pdbid, subset='calpha')
+        # here's the new bit        
+        if structure is None:
+            NoStructure = ([])
+            NoStructure.append(pdbid)
+            continue
+        
+        mappings = mapOntoChain(chain, ref_chain)
+        if len(mappings) == 0:
+            unmapped = ()
+            unmapped.append(pdbid)
+            continue
+        
+        atommap = mappings[0][0]
+        ensemble.addCoordset(atommap, weights=atommap.getFlags('mapped'))
+ 
+print NoStructure       
+print unmapped
+       
+        
+        
+repr(ensemble)
+
+## in the tutorial it says len(ensemble) == len(pdbfiles), but in this case, len(ensemble should be equal to the sum of the number of chains?
+
+ensemble.iterpose()
+
+closeLogfile('PhoQ_pca')
+
+
+writePDB('PhoQ_ensemble.pdb', ensemble)
+
+
+
